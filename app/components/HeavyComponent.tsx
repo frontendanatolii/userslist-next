08757a4978b3c1b7195288f16c8f6ac790bd6cac
Tolik Petras <toliketras4@gmail.com>
2023-09-13 10:22:43 +0300

    initial

---
+++ app/components/HeavyComponent.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const HeavyComponent = () => {
+  return (
+    <div className='text-black'>My Heavy Component</div>
+  )
+}
+
+export default HeavyComponent
---
+++ app/components/ProductCard.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import AddToCart from './AddToCart';
+
+const ProductCard = () => {
+  return (
+    <div className='text-black'>
+      <AddToCart />
+    </div>
+  )
+}
+
+export default ProductCard
